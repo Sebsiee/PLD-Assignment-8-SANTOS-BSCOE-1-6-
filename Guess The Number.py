3d907c8a177dd39a6256d1a418deff2a7fee07eb
Sebastian <santosbasty4@gmail.com>
2021-12-04 21:32:59 +0800

    Second Commit - Added Guess Counter

--- Guess The Number.py
+++ Guess The Number.py
@@ -5,21 +5,27 @@ def introduction():
 
 def lottery():
     generatedNumber = random.randint(0,100)
+    print(f"{generatedNumber}")
+    count = 0
     while True:
         try:
-            userNumber = int(input("Input number: "))
+            userNumber = int(input("Input number: ")) 
         except ValueError:
             print("Sorry, I didn't understand that.")
             continue
         if int(userNumber) < generatedNumber:
             print("Greater than")
+            count+=1 
             continue
         elif int(userNumber) > generatedNumber:
             print("Less than")
+            count+=1 
             continue
         else:
             print("You guessed correctly!")
+            print(f"You tried {count} times.")
             break
+
 
 introduction()
 lottery()
